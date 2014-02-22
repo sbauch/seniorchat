66c9467a26d1ee1cddc1b51f3c7b8d2d99ea2a38
Sam Bauch <sbauch@vaynermedia.com>
2014-02-22 15:48:07 -0500

    working with dashboard token

--- public/js/chat.js
+++ public/js/chat.js
@@ -21,14 +21,14 @@ var Chat = Parse.Object.extend("Chat");
 var query = new Parse.Query(Chat);
 
 	var apiKey    = "44662662";
-	var sessionId; //= "2_MX40NDY2MjY2Mn5-U2F0IEZlYiAyMiAxMjozMzo0MSBQU1QgMjAxNH4wLjY1MTIxNTQzfg";
+	var sessionId = "2_MX40NDY2MjY2Mn5-U2F0IEZlYiAyMiAxMjozMzo0MSBQU1QgMjAxNH4wLjY1MTIxNTQzfg";
 	var token     = "T1==cGFydG5lcl9pZD00NDY2MjY2MiZzZGtfdmVyc2lvbj10YnJ1YnktdGJyYi12MC45MS4yMDExLTAyLTE3JnNpZz00N2NiOGY4MGMzMjI2ZWUzYzU0OTY5YmJlMmI5NWVhMDM4YjU3MjJhOnJvbGU9cHVibGlzaGVyJnNlc3Npb25faWQ9Ml9NWDQwTkRZMk1qWTJNbjUtVTJGMElFWmxZaUF5TWlBeE1qb3pNem8wTVNCUVUxUWdNakF4Tkg0d0xqWTFNVEl4TlRRemZnJmNyZWF0ZV90aW1lPTEzOTMxMDEyMzImbm9uY2U9MC4yNzAyODU1OTU1MjczMDU2JmV4cGlyZV90aW1lPTEzOTU2OTMyMTYmY29ubmVjdGlvbl9kYXRhPQ==";
 
 query.get(chatID, {
     success: function(chat) {
         // The object was retrieved successfully.
         console.log(chat.get('sessionId'));
-        sessionId = chat.get('sessionId');
+        // sessionId = chat.get('sessionId');
 				// sessionId = '2_MX40NDY2MjY2Mn5-U2F0IEZlYiAyMiAxMjozMToxOCBQU1QgMjAxNH4wLjk3NDM5MzA3fg'
 				console.log(sessionId);
         
@@ -44,13 +44,14 @@ query.get(chatID, {
 				
 				  var publisher = TB.initPublisher(apiKey);
   				var session   = TB.initSession(sessionId);
- 
+ 					console.log(session);
+					// break;
   				session.connect(apiKey, token);
-  				session.addEventListener("sessionConnected", 
-                           sessionConnectedHandler);
- 
-  				session.addEventListener("streamCreated", 
-                           streamCreatedHandler);
+  				// session.addEventListener("sessionConnected", 
+  				//                            sessionConnectedHandler);
+  				//  
+  				//   				session.addEventListener("streamCreated", 
+  				//                            streamCreatedHandler);
 
 
 
