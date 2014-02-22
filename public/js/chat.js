f01bac1171fd2144de305027eaa9bda2f0a6fd3d
Sam Bauch <sbauch@vaynermedia.com>
2014-02-22 15:54:52 -0500

    working with dashboard token

--- public/js/chat.js
+++ public/js/chat.js
@@ -24,6 +24,9 @@ var query = new Parse.Query(Chat);
 	var sessionId = "2_MX40NDY2MjY2Mn5-U2F0IEZlYiAyMiAxMjozMzo0MSBQU1QgMjAxNH4wLjY1MTIxNTQzfg";
 	var token     = "T1==cGFydG5lcl9pZD00NDY2MjY2MiZzZGtfdmVyc2lvbj10YnJ1YnktdGJyYi12MC45MS4yMDExLTAyLTE3JnNpZz00N2NiOGY4MGMzMjI2ZWUzYzU0OTY5YmJlMmI5NWVhMDM4YjU3MjJhOnJvbGU9cHVibGlzaGVyJnNlc3Npb25faWQ9Ml9NWDQwTkRZMk1qWTJNbjUtVTJGMElFWmxZaUF5TWlBeE1qb3pNem8wTVNCUVUxUWdNakF4Tkg0d0xqWTFNVEl4TlRRemZnJmNyZWF0ZV90aW1lPTEzOTMxMDEyMzImbm9uY2U9MC4yNzAyODU1OTU1MjczMDU2JmV4cGlyZV90aW1lPTEzOTU2OTMyMTYmY29ubmVjdGlvbl9kYXRhPQ==";
 
+var session;
+var publisher;
+
 query.get(chatID, {
     success: function(chat) {
         // The object was retrieved successfully.
@@ -42,17 +45,17 @@ query.get(chatID, {
 				//         session.addEventListener("streamCreated",
 				//             streamCreatedHandler);
 				
-				  var publisher = TB.initPublisher(apiKey);
-  				var session   = TB.initSession(sessionId);
- 					console.log(session);
-					// break;
-  				session.connect(apiKey, token);
-  				// session.addEventListener("sessionConnected", 
-  				//                            sessionConnectedHandler);
-  				//  
-  				//   				session.addEventListener("streamCreated", 
-  				//                            streamCreatedHandler);
-
+			  publisher = TB.initPublisher(apiKey);
+ 				
+				session   = TB.initSession(sessionId);
+				console.log(session);
+ 				session.connect(apiKey, token);
+ 				session.addEventListener("sessionConnected", 
+ 				                           sessionConnectedHandler);
+ 				 
+ 				session.addEventListener("streamCreated", 
+ 				                           streamCreatedHandler);
+				session.publish(publisher);
 
 
     },
