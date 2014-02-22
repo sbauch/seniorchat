56ac0693722a0dd5d10abeb8a4cc85f5cdcf6b65
jin ruan <jruan6377@bths.edu>
2014-02-22 17:28:24 -0500

    added new pages

--- public/js/index.js
+++ public/js/index.js
@@ -1,23 +1,23 @@
-/**
- * Created by user on 2/22/14.
- */
-
-Parse.initialize("SsKj0DVPOxnQde6rHobJsUH21T2FwzlQbcDkUmGU", "wZ6GNIGQwQ030sGc6maS54fHjagJKAJXbhlxL1am");
-
-
-var Chat = Parse.Object.extend("Chat");
-var chat = new Chat();
-chat.set('participant_1', "wK38A5KSdh");
-chat.save(null, {
-    success: function(chat) {
-        // Execute any logic that should take place after the object is saved.
-        alert('New object created with objectId: ' + chat.id);
-    },
-    error: function(chat, error) {
-        // Execute any logic that should take place if the save fails.
-        // error is a Parse.Error with an error code and description.
-        alert('Failed to create new object, with error code: ' + error.description);
-
-    }
-});
-
+///**
+// * Created by user on 2/22/14.
+// */
+//
+//Parse.initialize("SsKj0DVPOxnQde6rHobJsUH21T2FwzlQbcDkUmGU", "wZ6GNIGQwQ030sGc6maS54fHjagJKAJXbhlxL1am");
+//
+//
+//var Chat = Parse.Object.extend("Chat");
+//var chat = new Chat();
+//chat.set('participant_1', "wK38A5KSdh");
+//chat.save(null, {
+//    success: function(chat) {
+//        // Execute any logic that should take place after the object is saved.
+//        alert('New object created with objectId: ' + chat.id);
+//    },
+//    error: function(chat, error) {
+//        // Execute any logic that should take place if the save fails.
+//        // error is a Parse.Error with an error code and description.
+//        alert('Failed to create new object, with error code: ' + error.description);
+//
+//    }
+//});
+//
